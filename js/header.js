e4d42caf495b3790d0345ebd720535b79cd4b41f
happyfrog <47712398+applebloom1214@users.noreply.github.com>
2023-05-31 20:00:31 +0900

    test new sidebar
    java script 코드로의 전환을 위해 새로운 사이드바를 구상중

--- js/header.js
+++ js/header.js
@@ -1,8 +1,14 @@
 
 const hModal = document.querySelector('.hModal');
-console.log(hModal);
 const menuIcon = document.getElementById('menuicon');
 
+const sidebar_test = document.querySelector(".sidebar_test");
+const side_closeBtn = document.querySelector('.side_closeBtn');
+
+
+side_closeBtn.addEventListener('click', () => {
+        sidebar_test.style.left = "-300px";
+});
 
 menuIcon.addEventListener('click', (event) => {
         hModal.classList.toggle('show');
