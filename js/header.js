75db5066a60934f1b4bfb0ee26af9745e0b1c8f0
happyfrog <47712398+applebloom1214@users.noreply.github.com>
2023-06-03 19:58:35 +0900

    modify community.html
    커뮤니티 모바일 사이드바 수정,
    X 버튼 뿐 아니라 사이드바 활성화시 모달창을 클릭하여도 사이드바가 닫히도록 조정.

--- js/header.js
+++ js/header.js
@@ -6,6 +6,14 @@ const side_closeBtn = document.querySelector('.side_closeBtn');
 const side_openBtn = document.querySelector('.side_openBtn');
 
 
+hModal.addEventListener('click', (event) => {
+        if (event.target === hModal) {
+                hModal.classList.toggle('show');
+                sidebar.style.left = "-300px";
+        }
+});
+
+
 side_closeBtn.addEventListener('click', () => {
         sidebar.style.left = "-300px";
         hModal.classList.toggle('show');
